05841941610941eec6b28e720dfbf3e2062505da
Mert Yıldız <mert@Mert-MacBook-Pro.local>
2025-05-05 15:59:47 +0300

    fix: disable static analysis on route files

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,10 +1,12 @@
+export const dynamic = 'force-dynamic';
 import { type EmailOtpType } from '@supabase/supabase-js';
 import { type NextRequest } from 'next/server';
 import { createClient } from '@/utils/supabase/server';
 import { redirect } from 'next/navigation';
 
 export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url);
+  const url = request?.url ? new URL(request.url) : null;
+  const searchParams = url?.searchParams ?? new URLSearchParams();
   const token_hash = searchParams.get('token_hash');
   const type = searchParams.get('type') as EmailOtpType | null;
 
